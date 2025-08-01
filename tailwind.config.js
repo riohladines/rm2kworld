c2b0785a73d8a60165f4246a2dfdc2222c39a950
riohladines <rm2kworld@gmail.com>
2025-08-01 11:13:08 +0800

    designing update

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        beige: '#f5ebe0',
+        autumn: '#c92a2a',
+      },
+    },
+  },
+  plugins: [],
+};
